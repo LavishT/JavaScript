689a7d9ab4a828cc5b00014d221be46f85a7b8bc
LavishT <65906401+LavishT@users.noreply.github.com>
2020-07-02 17:46:21 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,23 +1,45 @@
-// JavaScript Document
+let ul = document.querySelector('ul');
+let movie = document.querySelector('input');
+let liked = document.querySelectorAll('input')[1];
+let submit = document.querySelector('button');
+let li = document.querySelector('li');
 
-//1.) Using ol as your starting element, target the first li element 
-let startElement = document.querySelector('ol');
-let firstChild = startElement.firstElementChild;
-console.log(firstChild);
+function Movies(name, year, type, actors) {
+    this.name = name,
+        this.year = year,
+        this.type = type,
+        this.actors = actors;
+        greet: function() {
+            alert('fgchdchnc');
+    }
+    let li = document.createElement('li');
+    li.textContent = ('The name of the movie is ' + name + '. The year of the release is ' + year + '. The type of the movie is ' + type + '. The main Characters include ' + actors + '.');
 
-//2.) using main as your starting element, what is the last child? console.log to check it out 
-let startElement2 = document.querySelector('main');
-let lastChild = startElement2.lastElementChild;
-console.log(lastChild);
+    ul.appendChild(li);
+}
 
+let Survey1 = new Movies('3 idiots', '2009', 'Comedy', 'Aaamir Khan').greeting;
+let Survey2 = new Movies('ChakDe! India', '2007', 'Drama, Family, Sports', 'Shah Rukh Khan');
+let Survey3 = new Movies('Black', '2005', 'Drama', 'Amitabh Bachan');
+let Survey4 = new Movies('Munna Bhai M.B.B.S', '2003', 'Comedy, Drama, Musical', 'Sanjay Dutt');
+let Survey5 = new Movies('Avengers End Game', '2019', 'Action, Suspense', 'alottttttt');
 
-//3.) using html as your starting element, what is the last Element child? Console.log to check it out. 
-let startElement3 = document.querySelector('html');
-let lastChild2 = startElement3.lastElementChild;
-console.log(lastChild2);
+submit.addEventListener('click', showpara);
 
+function showpara() {
+    if (movie.value.length < 1) {
+        alert('Please Insert a Movie Name. I know it is hard to remember at times....')
+    } else if (liked.value.length < 1) {
 
-//4.) using the second li element as your starting element, target the next li element and change the colour to purple. 
-let startElement4 = document.querySelectorAll('li')[1];
-let nextElement = startElement4.nextElementSibling;
-nextElement.innerHTML = '<font color = "Purple">The Persian.</font>';
+        alert('Please enter what was the best thing you liked about that movie.????')
+    } else {
+        let moviee = movie.value;
+        let favthing = liked.value;
+        let li = document.createElement('li');
+        let textContent = ('Users Favourite movie is ' + moviee + '. The thing which user loved the most is ' + favthing);
+        li.append(textContent);
+        ul.appendChild(li);
+        movie.focus();
+        movie.select();
+    }
+}
